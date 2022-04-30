3fd28f68041ae7ea48fac765a2cbbbd96c1278ab
joshuavd <joshuavd@att.net>
2022-04-29 19:57:06 -0700

    added yahoo finance API and ability to pull from any ticker

--- rnn.py
+++ rnn.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential, load_model
 from keras.layers import LSTM, Dense, Dropout
+import yfinance as yf
+from pandas_datareader import data as pdr
+import sys
+
+yf.pdr_override()
 
 def create_dataset(df):
     x = []
@@ -15,55 +20,82 @@ def create_dataset(df):
     y = np.array(y)
     return x,y
 
-df = pd.read_csv('TSLA.csv')
 
-df = df['Open'].values
-df = df.reshape(-1, 1)
+def main():
 
-dataset_train = np.array(df[:int(df.shape[0]*0.8)])
-dataset_test = np.array(df[int(df.shape[0]*0.8):])
-print(dataset_train.shape)
-print(dataset_test.shape)
+    if len(sys.argv) == 1:
+        print("Must provide a ticker")
+        exit()
+    elif len(sys.argv) > 2:
+        print("Too many arguments.")
+        exit()
+    else:
+        t = sys.argv[1]
 
-scaler = MinMaxScaler(feature_range=(0,1))
+    ticker = yf.Ticker(t)
+    info = None
 
-dataset_train = scaler.fit_transform(dataset_train)
-dataset_train[:5]
+    try:
+        info = ticker.info
+    except:
+        print("Invalid ticker: " + t)
+        print("Exiting...")
+        exit()
+    
+    df = pdr.get_data_yahoo(t, start="2010-07-01", end="2021-01-30")
 
-dataset_test = scaler.transform(dataset_test)
-dataset_test[:5]
+    # df = pd.read_csv('TSLA.csv')
 
-x_train, y_train = create_dataset(dataset_train)
-x_test, y_test = create_dataset(dataset_test)
+    df = df['Open'].values
+    df = df.reshape(-1, 1)
 
-model = Sequential()
-model.add(LSTM(units=96, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=96, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=96, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=96))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
+    dataset_train = np.array(df[:int(df.shape[0]*0.8)])
+    dataset_test = np.array(df[int(df.shape[0]*0.8):])
+    print(dataset_train.shape)
+    print(dataset_test.shape)
 
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    scaler = MinMaxScaler(feature_range=(0,1))
 
-model.compile(loss='mean_squared_error', optimizer='adam')
+    dataset_train = scaler.fit_transform(dataset_train)
+    dataset_train[:5]
 
-model.fit(x_train, y_train, epochs=50, batch_size=32)
-model.save('stock_prediction.h5')
+    dataset_test = scaler.transform(dataset_test)
+    dataset_test[:5]
 
-model = load_model('stock_prediction.h5')
+    x_train, y_train = create_dataset(dataset_train)
+    x_test, y_test = create_dataset(dataset_test)
 
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
-y_test_scaled = scaler.inverse_transform(y_test.reshape(-1, 1))
+    model = Sequential()
+    model.add(LSTM(units=96, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=96, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=96, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=96))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
 
-fig, ax = plt.subplots(figsize=(16,8))
-ax.set_facecolor('#000041')
-ax.plot(y_test_scaled, color='red', label='Original price')
-plt.plot(predictions, color='cyan', label='Predicted price')
-plt.legend()
-plt.savefig('graph.png')
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    model.fit(x_train, y_train, epochs=50, batch_size=32)
+    model.save('stock_prediction2.h5')
+
+    model = load_model('stock_prediction2.h5')
+
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
+    y_test_scaled = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+    fig, ax = plt.subplots(figsize=(16,8))
+    ax.set_facecolor('#000041')
+    ax.plot(y_test_scaled, color='red', label='Original price')
+    plt.plot(predictions, color='cyan', label='Predicted price')
+    plt.legend()
+    plt.savefig('graph2.png')
+
+if __name__ == "__main__":
+    main()
